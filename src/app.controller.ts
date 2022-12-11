479e5b6077030555204d7345dfc33d2d6213af4c
Euphoria99 <363pavanbhat@gmail.com>
2022-12-11 22:04:02 +0530

    Initial setup

---
+++ src/app.controller.ts
@@ -0,0 +1,18 @@
+import { Controller, Get, Header } from '@nestjs/common';
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+
+  // @Get('hi')
+  // @Header('Content-type', 'application/json')
+  // getHi(): { name: string } {
+  //   return { name: 'Pavan Bhat' };
+  // }
+}
